0d5aaa9c46f9ce2e23c2083b273c95ce8ad08f9d
yomidep <noabiliaminfirst@gmail.com>
2023-11-26 15:48:53 +0100

    cleared some issues

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,14 +7,12 @@ require('@openzeppelin/hardhat-upgrades');
 /** @type import('hardhat/config').HardhatUserConfig */
 
 
-const PRIVATE_KEY = '73839b7947b85348a02e51c9fd8e2f13e0b0ef237555034003b34c5d65f8e214'
-
 module.exports = {
  solidity: "0.8.10",
  networks: {
   goerli: {
-    url : 'https://eth-goerli.g.alchemy.com/v2/QBDv73vmYqTJl9rEI-Y5aE99RinVsKJ0',
-    accounts: [PRIVATE_KEY],
+    url : [process.env.GOERLI_RPC_URL],
+    accounts: [process.env.PRIVATE_KEY],
   },
 }
 };
